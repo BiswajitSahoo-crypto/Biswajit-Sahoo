66bf28d1a804df3b9228680c01c1e33c18a2755f
Biswajit <biswajitsahoo110@gmail.com>
2021-05-21 15:04:22 +0530

    code change

---
+++ src/main/java/com/Gandhigroup/SimpleSprigbootAppApplication.java
@@ -0,0 +1,13 @@
+package com.Gandhigroup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSprigbootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSprigbootAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Gandhigroup/SimpleSprigbootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Gandhigroup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSprigbootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
